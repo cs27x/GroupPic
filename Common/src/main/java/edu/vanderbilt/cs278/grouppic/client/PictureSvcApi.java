f9ca7eb7b2cc7343471336aea4a32809f9b7c20c
Jejo Koola <jdkoola@gmail.com>
2014-11-03 10:40:55 -0600

    Cleaned up the PictureSvcApi

--- Common/src/main/java/edu/vanderbilt/cs278/grouppic/client/PictureSvcApi.java
+++ Common/src/main/java/edu/vanderbilt/cs278/grouppic/client/PictureSvcApi.java
@@ -5,14 +5,12 @@ import java.util.Collection;
 import edu.vanderbilt.cs278.grouppic.repository.Comment;
 import edu.vanderbilt.cs278.grouppic.repository.Picture;
 import edu.vanderbilt.cs278.grouppic.repository.PicturePreview;
+
 import retrofit.http.Body;
-import retrofit.http.DELETE;
 import retrofit.http.GET;
 import retrofit.http.POST;
 import retrofit.http.Path;
-import retrofit.http.Multipart;
-import retrofit.http.Part;
-import retrofit.mime.TypedFile;
+import retrofit.http.DELETE;
 
 /**
  * Created by andrewbachman on 10/28/14.
@@ -24,24 +22,16 @@ public interface PictureSvcApi {
 
     public static final String PICTURE_PATH = "/picture";
 
-        public static final String COMMENT_PATH = "/comments";
+    public static final String COMMENT_PATH = "/comments";
 
     public static final String POST_COMMENT_PATH = PICTURE_PATH + "/comment";
 
     @GET(PICTURE_PATH)
     public Collection<Picture> getPictureList();
 
-    /*
-    @Multipart
     @POST(PICTURE_PATH)
-    public void sendImageFile(@Part("photo") TypedFile photo, @Part("description") String description);
-    */
-    @POST(PICTURE_PATH)
-    public Void sendPicture(@Body Picture p);
+    public void sendPicture(@Body Picture p);
 
-	@DELETE(PICTURE_PATH + "/{id}")
-	public void deletePicture(@Path("id") long id);
-	
     @GET(PICTURE_PATH + "/{id}")
     public Picture getPictureWithId(@Path("id") long id);
 
@@ -50,5 +40,8 @@ public interface PictureSvcApi {
 
     @POST(POST_COMMENT_PATH)
     public void postComment(@Body Comment c);
+    
+    @DELETE(PICTURE_PATH + "/{id}")
+    public void deletePicture(@Path("id") long id);
 
 }
